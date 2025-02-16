f6f99557896698fd7e123ec1f7697130c640941c
dtsarev <czarev.1999@gmail.com>
2025-02-16 19:32:26 +0300

    config update

---
+++ src/config.py
@@ -0,0 +1,27 @@
+import yaml
+from pydantic import BaseModel
+from yaml.loader import SafeLoader
+
+
+def get_config(config_path: str):
+    # Open the file and load the file
+    with open(config_path, encoding='utf-8') as f:
+        data = yaml.load(f, Loader=SafeLoader)
+        return IConfig(**data)
+
+
+class Speech(BaseModel):
+    model: str
+    temperature: float
+    ans_max_length: int
+    max_tokens: int
+
+
+class IConfig(BaseModel):
+    """
+    Base config class
+    """
+    speech: Speech
+
+
+config = get_config('config.yaml')
